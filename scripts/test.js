ddac21574043ccef129c8c1de5c6078bc2d806d1
Robin Camille Davis <rdvs18@gmail.com>
2018-06-04 21:19:11 -0400

    Test script

---
+++ scripts/test.js
@@ -0,0 +1,26 @@
+module.exports = function(bot) {
+	//comes with commands specific to hubot module library
+	//see https://hubot.github.com/docs/scripting/ 
+
+	bot.hear(/Hello!/, function(res) {
+		return res.send("Bonjour!");
+	});
+
+	bot.hear(/Hi my name is (.*)/i, function(msg) {
+		var name = msg.match[1];
+		return msg.reply("Nice to meet ya, " + name);
+	});
+
+	bot.hear(/Add (.*) and (.*)/i, function(msg) {
+		var a = parseInt(msg.match[1]);
+		var b = parseInt(msg.match[2]);
+		var equation = a + b;
+		return msg.reply(a + " and " + b + "? That's " + equation);
+	})
+
+	//below doesn't work -- bug
+	bot.respond(/What's your fave food\?/, function(res) {
+		return res.send("Brains");
+	});
+
+};
