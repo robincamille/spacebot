7ed4a268a03edf92dd7d091c80e077ef34f59772
Robin Camille Davis <rdvs18@gmail.com>
2018-06-04 21:25:44 -0400

    Try random

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,12 @@ module.exports = function(bot) {
 	//comes with commands specific to hubot module library
 	//see https://hubot.github.com/docs/scripting/ 
 
+	// ['Object','Period (days)','Distance (ly)']
+	exoplanets = ['Proxima Centauri b','11.186','4.22'],['Gliese 667 Cc','28.143 ± 0.029','23.62'],['Kepler-442b','112.3053','1291.6'],['Kepler-452b','384.8','1402'],['Wolf 1061c','17.9','13.8'],['Kepler-1229b','86.8','769'],['Kapteyn b*','48.6','13'],['Kepler-62f','267.291','1200'],['Kepler-186f','129.9459','561'],['Luyten b','18.65','12.36'],['TRAPPIST-1d','4.05','39'],['TRAPPIST-1e','6.1','39'],['TRAPPIST-1f','9.2','39'],['TRAPPIST-1g','12.4','39'],['LHS 1140 b','25','40'],['Kepler-1638b','259.365','2491.83']
+
 	bot.hear(/I want to leave this planet/, function(res) {
-		return res.send("How about a nice exoplanet?");
+		planet = res.random(exoplanets);
+		return res.send("How about " + planet[0] + "?");
 	});
 
 	bot.hear(/Hi my name is (.*)/i, function(msg) {
